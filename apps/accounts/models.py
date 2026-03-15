62a237464e66ff2f96fe31e95d7da2a5f2a62268
JhankarRawal <jhankarrawal182@gmail.com>
2026-03-15 13:41:50 +0545

    third deploy

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -36,3 +36,12 @@ def create_or_update_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
     else:
         Profile.objects.get_or_create(user=instance)
+        
+from django.contrib.postgres.operations import TrigramExtension
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    operations = [
+        TrigramExtension(),
+    ]
